7a99a3f0ab5a6dc26a59637f63c881183bb31b53
Hemanta <hemantabhoi16112003@gmail.com>
2024-07-18 23:38:37 +0530

    MongoDB learning started

---
+++ MongoDB/1_mongodb.js
@@ -0,0 +1,11 @@
+/*
+--- To run mongosh in command prompt ---:
+-> Command: mongosh
+-- Basic Commands:
+    : cls - clear
+    : help - get to know about some basic commands
+    : quit or exit - to exit from the shell
+
+
+
+*/
